90a641f001798a020cb96452393d2f3f31bf742a
Graham Ollis <plicease@cpan.org>
2026-08-06 09:54:23 -0600

    Initial structure

---
+++ lib/Tie/Google/Sheets.pm
@@ -0,0 +1,9 @@
+use warnings;
+use 5.042;
+use experimental qw( defer try );
+use true;
+
+package Tie::Google::Sheets {
+
+    # ABSTRACT: Tie perl variables to Google Sheets
+}
---
+++ t/tie_google_sheets.t
@@ -0,0 +1,6 @@
+use Test2::V0 -no_srand => 1;
+use Tie::Google::Sheets;
+
+ok 1, 'todo';
+
+done_testing;
